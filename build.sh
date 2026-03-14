b7dfd7bddb66f3956ae7ef7dbe450e56f15c5a7e
Sajin1119 <sajinsatheesan06@gmail.com>
2026-03-14 22:20:06 +0530

    fix: create superuser on render

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
+
 #!/usr/bin/env bash
 pip install -r requirements.txt
 pip install gunicorn
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from users.models import User
+u, created = User.objects.get_or_create(username='sajin', defaults={'email':'sajin@test.com'})
+u.is_staff = True
+u.is_superuser = True
+u.set_password('Sajin@1234')
+u.save()
+print('Superuser ready!')
+"
